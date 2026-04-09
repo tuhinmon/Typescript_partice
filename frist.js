57dc9e9821a586c4025cc84634579209c1fa2a5e
Tuhin Mondal <tuhinmondal746@gmail.com>
2026-04-09 21:44:53 +0530

    declear premitive datatype

--- frist.js
+++ frist.js
@@ -1,1 +1,10 @@
-let a = 12;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// how declear premtive datatype in typescript
+let num = 12;
+let str = "Tuhin";
+let bool = true;
+let undi;
+let nul = null;
+let bigint = 2323232323232323n;
+//# sourceMappingURL=frist.js.map
--- frist.ts
+++ frist.ts
@@ -1,1 +1,8 @@
-let a: number = 12;
+// how declear premtive datatype in typescript
+let num: number = 12;
+let str: string = "Tuhin";
+let bool:boolean = true;
+let undi : undefined ;
+let nul:null = null
+let bigint : bigint = 2323232323232323n;
+
